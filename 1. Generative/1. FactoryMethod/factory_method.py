be85c19452658f161410a4d3f15df9d9254b3e10
Telelichko <52406628+Telelichko@users.noreply.github.com>
2022-06-20 00:39:16 +0700

    Added factory method pattern info

---
+++ 1. Generative/1. FactoryMethod/factory_method.py
@@ -0,0 +1,34 @@
+class IProduct:
+    def release(self):
+        pass
+
+class Car(IProduct):
+    def release(self):
+        print('Выпущен легковой автомобиль.')
+
+class Truck(IProduct):
+    def release(self):
+        print('Выпущен грузовой автомобиль.')
+
+class IWorkShop:
+    def create(self) -> IProduct:
+        pass
+
+class CarWorkShop(IWorkShop):
+    def create(self):
+        return Car()
+
+class TruckWorkShop(IWorkShop):
+    def create(self):
+        return Truck()
+
+
+if __name__ == '__main__':
+    creator = CarWorkShop()
+    car = creator.create()
+
+    creator = TruckWorkShop()
+    truck = creator.create()
+
+    car.release()
+    truck.release()
